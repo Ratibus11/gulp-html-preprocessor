668bf8aea593d7e5e0a7425c013eb97163b3fcc1
Ratibus11 <49524093+Ratibus11@users.noreply.github.com>
2022-05-28 16:22:37 +0200

    Pipe processing

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
 // Utils
-import { glob } from "./utils/glob";
 import { preprocessorVariables } from "./types";
+import { obj, TransformCallback } from "through2";
 
 // Preprocessor
-import { process as processHTML } from "./preprocessors/process";
+import { process as processFile } from "./preprocessors/process";
 
 /**
  * Extension's entry point. Process all files which matches with the given globs.
- * @param globs Globs to process.
  * @param options Variables to process. Can be strings, numbers or booleans.
  */
-function process(
-	globs: string[] | string,
-	variables?: preprocessorVariables
-): void {
-	glob(globs).forEach((match) => {
-		processHTML(match, variables || {});
-	});
+function process(options: preprocessorVariables = {}) {
+	return obj(
+		(file: any, encoding: BufferEncoding, callback: TransformCallback) => {
+			let data = file.contents.toString(encoding);
+			data = processFile(data, options);
+			file.contents = Buffer.from(data);
+
+			callback(null, file);
+		}
+	);
 }
 
 export { process };
--- src/preprocessors/process.ts
+++ src/preprocessors/process.ts
@@ -2,19 +2,14 @@ import { processComments } from "./comment";
 import { processTags } from "./tagAttribute";
 import { preprocessorVariables } from "../types";
 
-import { load } from "../utils/file";
-
 /**
  * Execute HTML preprocessor on the given path with given variables.
  * @param path HTML file to process.
  * @param variables Variables to use during the process.
  */
-function process(path: string, variables: preprocessorVariables): string {
-	var data = load(path);
-
+function process(data: string, variables: preprocessorVariables): string {
 	data = processComments(data, variables);
 	data = processTags(data);
-	console.log(data);
 
 	return data;
 }
